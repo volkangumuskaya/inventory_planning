f7732bea2d9be01c87d903e82bc6f3f3d393f549
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-10-24 14:38:45 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import random
+random.seed(42)
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
@@ -19,9 +21,9 @@ with st.sidebar:
 #Show measurements only for selected station
 st.header('Problem parameters', divider=True)
 
-n_resources = st.selectbox(
-    'Select station for latest measurements',
-    [1,2,3])
+n_resources = st.selectbox('# resources',list(range(1,3)))
+n_products = st.selectbox('# products',list(range(1,10)))
+n_orders = st.selectbox('# orders',list(range(1,30)))
 
 from_year, to_year = st.select_slider(
         "Select a range of date",
