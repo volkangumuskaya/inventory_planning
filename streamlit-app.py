20b841784687fb9772a4276f30a7423443187ede
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-10-24 14:28:01 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,1 +1,16 @@
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='volkan-ai',
+    layout="wide",
+    page_icon=':rainbow:', # This is an emoji shortcode. Could be a URL too.
+    # page_icon="images/weather_icon.png"
+)
+
+st.sidebar.header("About",divider='orange')
+with st.sidebar:
+    st.image('images/profile_round.png',width=200,caption="https://www.linkedin.com/in/volkangumuskaya/")
+    
+'''
+# Example app
+'''
