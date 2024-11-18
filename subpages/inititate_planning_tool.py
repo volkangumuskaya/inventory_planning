b6fc7c920a2ffdaafa1cb5976e586b29a6025564
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-11-18 13:31:33 +0100

    Update inititate_planning_tool.py

--- subpages/inititate_planning_tool.py
+++ subpages/inititate_planning_tool.py
@@ -1,13 +1,13 @@
 import streamlit as st
 # Import the PuLP library
 import pandas as pd
-from pulp import LpProblem, LpMinimize, LpVariable, lpSum,LpInteger,LpContinuous,LpBinary,LpStatus,value
-import random
-from collections import defaultdict
-import pickle
-import os
-import sys
-from pathlib import Path
+# from pulp import LpProblem, LpMinimize, LpVariable, lpSum,LpInteger,LpContinuous,LpBinary,LpStatus,value
+# import random
+# from collections import defaultdict
+# import pickle
+# import os
+# import sys
+# from pathlib import Path
 
 st.write("hi first")
 st.write("here is first version:",pd.__version__)
