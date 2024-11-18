b9309795733356239d19e0f25ed43daac5afa8a3
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-11-18 10:25:54 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from pages import inititate_planning_tool
-
+# Set the title and favicon that appear in the Browser's tab bar.
+    st.set_page_config(
+        page_title='volkan-ai',
+        layout="wide",
+        # page_icon="images/weather_icon.png"
+    )
 # Sidebar navigation
 st.sidebar.title("Navigation")
 page = st.sidebar.radio(
