50df8020c46f3dd5bf3bc85d26a2f4ec16459899
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-11-18 10:21:10 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,17 +1,19 @@
 import streamlit as st
+import inititate_planning_tool
 
-# Sidebar navigation with direct clickable options
-page = st.sidebar.radio("Navigation", ["Home", "About", "Contact"])
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio(
+    "",
+    ["Home", "About", "Contact"]
+)
 
 # Show content based on the selected page
 if page == "Home":
-    st.title("Home Page")
-    st.write("Welcome to the home page!")
-
+    initiate_planning.show()
 elif page == "About":
     st.title("About Page")
     st.write("This is the about page.")
-
 elif page == "Contact":
     st.title("Contact Page")
-    st.write("Contact us here!")
+    st.write("This is the contact page.")
