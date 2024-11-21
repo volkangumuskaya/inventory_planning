2b2a94ae65c46d1b213d85a61467a9f0333df1cf
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-11-21 15:01:40 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,21 +10,21 @@ st.set_page_config(
 st.sidebar.title("Navigation")
 page = st.sidebar.radio(
     "",
-    ["Home", "About","Module_matching"]
+    ["Solve_model","Module_matching"]
 )
 
 # import os
 # print("Main Current Working Directory:", os.getcwd())
 
 # Show content based on the selected page
-if page == "Home":
+if page == "Solve_model":
     # test_page.show()
     inititate_planning_tool.show()
 elif page == "About":
     st.title("About Page")
     st.write("This is the about page.")
 elif page == "Module_matching":
-    st.title("Module matching",divider=True)
+    st.header("Module matching",divider=True)
     module_matching.show()
 # elif page == "Contact":
 #     test_page.show()
