2a0f29cdeb10fb429c48112af554548c62ef5707
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-12-14 14:32:10 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -21,79 +21,6 @@ from subpages.prioritize_order import check_delayed_orders
 from subpages.model_functions import create_main_objects,create_obj_function,create_model
 from subpages.prioritize_order import add_objective_terms_v2
 
-# st.write('before params')
-#Set of periods
-n_time_period=20
-n_resource=2
-n_customer=3
-n_order=500
-min_q_per_order=40
-max_q_per_order=70
-min_criticality=4
-max_criticality=10
-seed=50
-random.seed(seed)
-
-
-# st.write('after page config')
-##GENERATE MAIN COMPONENTS RANDOMLY
-time_ids, resources, products, customers, orders = create_main_objects(
-    n_period=n_time_period, n_resource=n_resource, n_customer=n_customer, n_order=n_order,
-    min_q_per_order=min_q_per_order, max_q_per_order=max_q_per_order, seed=seed)
-prob, x, y, inv = create_model(
-    resources=resources, products=products, customers=customers, orders=orders,
-    time_ids=time_ids, min_criticality=min_criticality, max_criticality=max_criticality, seed=seed)
-prob_tmp, x_tmp, y_tmp, inv_tmp = create_model(
-    resources=resources, products=products, customers=customers, orders=orders,
-    time_ids=time_ids, min_criticality=min_criticality, max_criticality=max_criticality, seed=seed)
-prob.solve()
-# st.write('after solve')
-orders = retrieve_fulfill_times(
-        orders_tmp=orders,
-        y_vars=y,
-        time_periods=time_ids)
-delayed_orders, total_delayed_units = check_delayed_orders(
-        f_orders=orders, f_y=y, f_time_ids=time_ids)
-
-total_quantity_per_product = determine_total_quantity_per_product(orders_f=orders, products_f=products)
-total_quantity_all_products = sum(total_quantity_per_product.values())
-
-# Decision variables and indices
-resource_ids = [x.resource_id for x in resources]
-product_ids = [x.product_id for x in products]
-order_ids = [x.order_id for x in orders]
-resource_names = [x.name for x in resources]
-product_names = [x.name for x in products]
-order_names = [f'order_{x.order_id}' for x in orders]
-random.seed(seed)
-criticality = [random.randint(a=min_criticality, b=max_criticality) / max_criticality for x in orders]
-
-#Save to session state
-st.session_state.current_model = prob
-st.session_state.orders = orders
-st.session_state.delayed_orders = delayed_orders
-# st.write('init entered')
-st.session_state.total_delayed_units = total_delayed_units
-st.session_state.resources = resources
-st.session_state.products = products
-st.session_state.product_ids = product_ids
-st.session_state.order_ids = order_ids
-st.session_state.resource_ids = resource_ids
-st.session_state.seed=seed
-st.session_state.min_criticality=min_criticality
-st.session_state.max_criticality=max_criticality
-
-st.session_state.customers = customers
-st.session_state.time_ids = time_ids
-st.session_state.x = x
-st.session_state.y = y
-st.session_state.criticality = criticality
-
-# st.write("st.session_state.current_model initialized")
-st.session_state.tmp_model = prob_tmp
-# st.write("st.session_state.tmp_model initialized")
-
-
 # Set the title and favicon that appear in the Browser's tab bar.
 module_name_list = ['Initiate/create a plan',
                     'Modify the plan',
@@ -119,8 +46,80 @@ page = st.sidebar.radio(
 # print("Main Current Working Directory:", os.getcwd())
 # Initialize chat history and system prompt
 if "messages" not in st.session_state:
-  st.session_state.messages = []  # Start with an empty chat history
-  st.session_state.welcome_message_shown = False
+    st.session_state.messages = []  # Start with an empty chat history
+    st.session_state.welcome_message_shown = False
+    # st.write('before params')
+    #Set of periods
+    n_time_period=20
+    n_resource=2
+    n_customer=3
+    n_order=500
+    min_q_per_order=40
+    max_q_per_order=70
+    min_criticality=4
+    max_criticality=10
+    seed=50
+    random.seed(seed)
+    
+    
+    # st.write('after page config')
+    ##GENERATE MAIN COMPONENTS RANDOMLY
+    time_ids, resources, products, customers, orders = create_main_objects(
+        n_period=n_time_period, n_resource=n_resource, n_customer=n_customer, n_order=n_order,
+        min_q_per_order=min_q_per_order, max_q_per_order=max_q_per_order, seed=seed)
+    prob, x, y, inv = create_model(
+        resources=resources, products=products, customers=customers, orders=orders,
+        time_ids=time_ids, min_criticality=min_criticality, max_criticality=max_criticality, seed=seed)
+    prob_tmp, x_tmp, y_tmp, inv_tmp = create_model(
+        resources=resources, products=products, customers=customers, orders=orders,
+        time_ids=time_ids, min_criticality=min_criticality, max_criticality=max_criticality, seed=seed)
+    prob.solve()
+    # st.write('after solve')
+    orders = retrieve_fulfill_times(
+            orders_tmp=orders,
+            y_vars=y,
+            time_periods=time_ids)
+    delayed_orders, total_delayed_units = check_delayed_orders(
+            f_orders=orders, f_y=y, f_time_ids=time_ids)
+    
+    total_quantity_per_product = determine_total_quantity_per_product(orders_f=orders, products_f=products)
+    total_quantity_all_products = sum(total_quantity_per_product.values())
+    
+    # Decision variables and indices
+    resource_ids = [x.resource_id for x in resources]
+    product_ids = [x.product_id for x in products]
+    order_ids = [x.order_id for x in orders]
+    resource_names = [x.name for x in resources]
+    product_names = [x.name for x in products]
+    order_names = [f'order_{x.order_id}' for x in orders]
+    random.seed(seed)
+    criticality = [random.randint(a=min_criticality, b=max_criticality) / max_criticality for x in orders]
+    
+    #Save to session state
+    st.session_state.current_model = prob
+    st.session_state.orders = orders
+    st.session_state.delayed_orders = delayed_orders
+    # st.write('init entered')
+    st.session_state.total_delayed_units = total_delayed_units
+    st.session_state.resources = resources
+    st.session_state.products = products
+    st.session_state.product_ids = product_ids
+    st.session_state.order_ids = order_ids
+    st.session_state.resource_ids = resource_ids
+    st.session_state.seed=seed
+    st.session_state.min_criticality=min_criticality
+    st.session_state.max_criticality=max_criticality
+    
+    st.session_state.customers = customers
+    st.session_state.time_ids = time_ids
+    st.session_state.x = x
+    st.session_state.y = y
+    st.session_state.criticality = criticality
+    
+    # st.write("st.session_state.current_model initialized")
+    st.session_state.tmp_model = prob_tmp
+    # st.write("st.session_state.tmp_model initialized")
+  
 
 if page == "Model v2":
     st.header("Model v2",divider=True)
