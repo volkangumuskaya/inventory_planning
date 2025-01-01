292c8d7c5416a333db649e0d17261f6ab129fbc0
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2025-01-01 14:51:15 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     page_title='volkan-ai',
     layout="wide"
 )
-import subpages
+st.write('checkpoint1 streamlit-app.py')
 from subpages import module_matching # Corresponds to the page where we have modules, e.g. initalize, priortize orders, etc
 from subpages import default_model # Corresponds to creating and solving a math model with customized parameters
 
