1ac8bae8c1d08fe11d51b2e2f4315b914d1974ea
Volkan Gumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2025-01-01 15:49:06 +0100

    Update initialize.py

--- subpages/initialize.py
+++ subpages/initialize.py
@@ -2,7 +2,6 @@
 # Observe that in subpages.module_matching, we use st.session_state vars without initializing them.
 # If we run default_model page, there we do initialize. However, even if we activate module_matching straight away
 # these vars are already initialized, for example st.session_state.min_criticality.
-# This is achieved via this __init__.py
 # It is also where we format the streamlit page via st.set_page_config (st.set_page_config should always be the first
 # streamlit command ti be run. If we added a st.write('hello') before st.set_page_config, we would get errror).
 # If interested, some st.write's can be thrown in various points to check the order of execution.
